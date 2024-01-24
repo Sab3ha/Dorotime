5b216729c770ea9788a0cc4cdfe0ca1180408e20
q1225 <46804537+q1225@users.noreply.github.com>
2024-01-24 17:51:39 -0500

    Add files via upload

--- frontend/src/components/Timer.js
+++ frontend/src/components/Timer.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
+export default Timer;  
 const Timer = ({ time, isBreak }) => {
     return (
         <div>
             <p>{`${Math.floor(time / 60)}:${(time % 60).toString().padStart(2, '0')}`}</p>
         </div>
-    );
-};
-
-export default Timer;
+    )}
+  
+    if (Timer=="0:00"){
+       var snd = new Audio('/alarm.mp3');
+       snd.play();
+}
