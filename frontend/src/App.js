442fae964528799dea5d8fa02a6960533ef5ae5a
Sab3ha <sabehak321@gmail.com>
2024-01-28 00:34:48 -0500

    Fix alarm

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,54 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import Timer from './components/Timer.js';
 import Controls from './components/Controls.js';
-import './App.css'
+import './App.css';
 import music from './music.png';
-import sound_one from './Music/Calm Ocean Waves With Little Bird Song Sound Effect.mp3'
-import sound_two from './Music/Calm River.mp3'
-import sound_three from './Music/Cricket Chirping.mp3'
-import sound_four from './Music/Ocean Waves.mp3'
-import sound_five from './Music/Peacful Water Stream In Forest.mp3'
-import sound_six from './Music/Rain.mp3'
-import sound_seven from './Music/Wind In Tall Grass.mp3'
+import sound_one from './Music/Calm Ocean Waves With Little Bird Song Sound Effect.mp3';
+import sound_two from './Music/Calm River.mp3';
+import sound_three from './Music/Cricket Chirping.mp3';
+import sound_four from './Music/Ocean Waves.mp3';
+import sound_five from './Music/Peacful Water Stream In Forest.mp3';
+import sound_six from './Music/Rain.mp3';
+import sound_seven from './Music/Wind In Tall Grass.mp3';
 
-  const App = () => {
+const App = () => {
     const [time, setTime] = useState(1500);
     const [isBreak, setIsBreak] = useState(false);
     const [isActive, setIsActive] = useState(false);
     const [mode, setMode] = useState('pomodoro');
-    const playlist = [sound_one, sound_two, sound_three, sound_four, sound_five, sound_six, sound_seven];
-    const audioRef = useRef(new Audio()); // Manage audio element in state
+    const playlist = [
+        sound_one,
+        sound_two,
+        sound_three,
+        sound_four,
+        sound_five,
+        sound_six,
+        sound_seven,
+    ];
+    const audioRef = useRef(new Audio());
 
-  const play = () => {
-    // If the audio is playing, pause it
-    if (!audioRef.current.paused) {
-      audioRef.current.pause();
-    } else {
-      // If the audio is paused, play it
-      const randomIndex = Math.floor(Math.random() * playlist.length);//random playlist
-      const selectedTrack = playlist[randomIndex];
-      audioRef.current.src = selectedTrack;
-      audioRef.current.play();
-    }
-  };
+    const play = () => {
+
+        if (!audioRef.current.paused) {
+            audioRef.current.pause();
+        } else {
+
+            const randomIndex = Math.floor(Math.random() * playlist.length);
+            const selectedTrack = playlist[randomIndex];
+            audioRef.current.src = selectedTrack;
+            audioRef.current.play();
+        }
+    };
+
+    useEffect(() => {
+
+        audioRef.current.addEventListener('ended', play);
+
+        return () => {
+            audioRef.current.removeEventListener('ended', play);
+        };
+    }, []);
+
     useEffect(() => {
         let interval;
 
@@ -39,14 +57,24 @@ import sound_seven from './Music/Wind In Tall Grass.mp3'
                 setTime((prevTime) => prevTime - 1);
             }, 1000);
         } else if (isActive && time === 0) {
-            setIsBreak((prevIsBreak) => {
-                setTime(getNextTime(!prevIsBreak));
-                return !prevIsBreak;
+            setIsActive(false);
+
+
+            setMode((prevMode) => {
+                if (prevMode === 'pomodoro') {
+                    return 'short';
+                } else {
+                    return 'pomodoro';
+                }
             });
+
+            // Set the time based on the new mode
+            setTime(getNextTime(mode));
         }
 
         return () => clearInterval(interval);
-    }, [isActive, time]);
+    }, [isActive, time, mode]);
+
 
     const startTimer = () => {
         setIsActive(true);
@@ -67,43 +95,50 @@ import sound_seven from './Music/Wind In Tall Grass.mp3'
     };
 
     const getNextTime = (newMode) => {
-        return newMode === 'short' ? 300 : (newMode === 'long' ? 600 : 1500);
+        return newMode === 'short' ? 300 : newMode === 'long' ? 600 : 1500;
     };
-  return (
-      <div className="container">
-          <h1 className='dorotime'>Dorotime</h1>
-          
-          <div className="square-with-diamond">
-              <div className="diamond-inside">
-                  <div className="timer-text">
-                  <button onClick={play} className='music-btn'><img src={music} alt="Logo" /> </button>
-                      <Timer time={time} isBreak={isBreak}/>
-                      <Controls startTimer={startTimer} resetTimer={resetTimer} isActive={isActive}/>
-                  </div>
-              </div>
-          </div>
 
-          <div className="small-square-with-diamond bottom">
-              <div className="diamond-inside bottom">
-                  <button className="timer-text small" onClick={() => switchMode('long')}>Long Break</button>
-              </div>
-          </div>
+    return (
+        <div className="container">
+            <h1 className="dorotime">Dorotime</h1>
 
-          <div className="small-square-with-diamond right">
-              <div className="diamond-inside right">
-                  <button className="timer-text small" onClick={() => switchMode('short')}>Short Break</button>
-              </div>
-          </div>
+            <div className="square-with-diamond">
+                <div className="diamond-inside">
+                    <div className="timer-text">
+                        <button onClick={play} className="music-btn">
+                            <img src={music} alt="Logo" />
+                        </button>
+                        <Timer time={time} isBreak={isBreak} />
+                        <Controls startTimer={startTimer} resetTimer={resetTimer} isActive={isActive} />
+                    </div>
+                </div>
+            </div>
 
-          <div className="small-square-with-diamond top">
-              <div className="diamond-inside top">
-                  <button className="timer-text small" onClick={() => switchMode('pomodoro')}>Pomodoro</button>
-              </div>
-          </div>
+            <div className="small-square-with-diamond bottom">
+                <div className="diamond-inside bottom">
+                    <button className="timer-text small" onClick={() => switchMode('long')}>
+                        Long Break
+                    </button>
+                </div>
+            </div>
 
-      </div>
+            <div className="small-square-with-diamond right">
+                <div className="diamond-inside right">
+                    <button className="timer-text small" onClick={() => switchMode('short')}>
+                        Short Break
+                    </button>
+                </div>
+            </div>
 
-  );
+            <div className="small-square-with-diamond top">
+                <div className="diamond-inside top">
+                    <button className="timer-text small" onClick={() => switchMode('pomodoro')}>
+                        Pomodoro
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default App;
--- frontend/src/components/Timer.js
+++ frontend/src/components/Timer.js
@@ -1,16 +1,19 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import AlarmSound from '../Music/Alarm.mp3';
 
 const Timer = ({ time, isBreak }) => {
+    useEffect(() => {
+        if (time === 0) {
+            var snd = new Audio(AlarmSound);
+            snd.play();
+        }
+    }, [time]);
+
     return (
         <div>
             <p>{`${Math.floor(time / 60)}:${(time % 60).toString().padStart(2, '0')}`}</p>
         </div>
-    )}
-  
-    if (Timer==="0:00"){
-       var snd = new Audio('/alarm.mp3');
-       snd.play();
-}
+    );
+};
 
 export default Timer;
